5b73d3602762f251805841f96d17fcd74b1da6f4
Esperanza Moreno Cruz <hola@mi-mina.com>
2019-09-28 14:12:22 +0200

    Mi primera aplicación Django, primer commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/espe/Documents/programacion/djangogirls/djangotutorial/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
